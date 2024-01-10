e551260510d4826f86cb5bade246305870bd6deb
Abhishek Verma <107166866+abhitan@users.noreply.github.com>
2024-01-10 12:19:22 +0530

    Time: 516 ms (18.98%) | Memory: 292.6 MB (12.63%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-
-   int solve(vector<int>& nums,int n,int ind,int prev_ind, vector<vector<int>>&dp)
-   {
-       if(ind == n)
-       {
-           return 0;
-       }
-
-       if(dp[ind][prev_ind+1] != -1)
-       {
-           return dp[ind][prev_ind+1];
-       }
-
-       int len = solve(nums,n,ind+1,prev_ind,dp); // Not Take case
-       if(prev_ind == -1 || nums[ind] > nums[prev_ind])
-       len = max(len,1 + solve(nums,n,ind+1,ind,dp)); // Take Case
-
-       return dp[ind][prev_ind+1] = len;
-   }
-
-
-
-
-
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(nums,n,0,-1,dp);
+       int n = nums.size();
+       vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+       for(int ind = n-1;ind >= 0 ;ind--)
+       {
+           for(int prev_ind = ind-1;prev_ind >= -1;prev_ind--)
+           {
+               
+                int len = dp[ind+1][prev_ind+1]; // Not Take case
+                if(prev_ind == -1 || nums[ind] > nums[prev_ind])
+                len = max(len,1 + dp[ind+1][ind+1]); // Take Case
+
+               dp[ind][prev_ind+1] = len;
+           }
+       }
+       return dp[0][-1+1];
     }
 };
