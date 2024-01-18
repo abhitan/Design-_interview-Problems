fd58080fbbdaed051fe7b639d5e7b7a3757b8503
Abhishek Verma <107166866+abhitan@users.noreply.github.com>
2024-01-18 12:12:28 +0530

    Time: 6 ms (97.08%) | Memory: 29.9 MB (79.91%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
+        int n = nums.size();
         unordered_map<int,int>mp;
-        int maxi = INT_MIN;
-        for(auto it:nums)
+        for(auto &it:nums)
         {
             mp[it]++;
         }
 
-     for(auto it:mp) maxi = max(maxi,it.second);
-        vector<vector<int>>ans(maxi);
+        int maxi = INT_MIN;
         for(auto &it:mp)
+        {
+            maxi = max(maxi,it.second);
+        }
+
+        vector<vector<int>>ans(maxi);
+
+        for(auto it:mp)
         {
             int x = it.second;
             int val = it.first;
             int i = 0;
-
             while(x != 0)
             {
                 ans[i].push_back(val);
